70fd81280f7b0684c0d4b252bb7bfdf79bac8ab8
PuttaSurya <suryapavan7a77@gmail.com>
2025-04-30 11:01:57 +0530

    fix bug in apiurl

--- src/services/auth-request.service.ts
+++ src/services/auth-request.service.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'https://leaveapi.aultrapaints.com/api';
 
-//const API_BASE_URL = 'https://leaveapi.aultrapaints.com/api';
-
- const API_BASE_URL = 'http://localhost:5000/api'
+ //const API_BASE_URL = 'http://localhost:5000/api'
 
 export const AuthRequestService = {
   register: async (userData) => {
